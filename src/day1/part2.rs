7a31b660e792ccf2441100f9310af5e989c645db
foo-dogsquared <foo.dogsquared@gmail.com>
2019-12-07 02:01:04 +0800

    Update the second part of Day 1
    Now there is a dedicated function for calculating the required amount of fuel (with the recursive fuel requirement).
    
     I've also updated the test cases. I simply forgot about that.

--- src/day1/part2.rs
+++ src/day1/part2.rs
@@ -7,22 +7,8 @@ pub fn main () -> Result<i64> {
 
     for line in input.lines() {
         // initial fuel amount
-        let mut fuel: i64 = rocket_fuel_equation(line.parse::<i64>()?);
-        let mut fuel_vector: Vec<i64> = vec![fuel];
-
-        // calculating the fuel requirement of further fuel
-        loop {
-            let fuel_requirement = rocket_fuel_equation(fuel);
-            if fuel_requirement > 0 {
-                fuel = fuel_requirement;
-                fuel_vector.push(fuel);
-            }  else {
-                break;
-            }
-        }
-
-        let total_fuel_requirement: i64 = fuel_vector.iter().sum();
-        total_fuel += total_fuel_requirement;
+        let fuel: i64 = rocket_fuel_equation_with_inner_fuel(line.parse::<i64>()?);
+        total_fuel += fuel;
     }
 
     Ok(total_fuel)
@@ -32,15 +18,38 @@ pub fn rocket_fuel_equation (mass: i64) -> i64 {
     (mass / 3) - 2
 }
 
+/// Calculates the rocket fuel needed considering the fuel itself. 
+/// 
+/// Even though this could easily be a recursive function, I try to make it possible only with loops. 
+pub fn rocket_fuel_equation_with_inner_fuel (mass: i64) -> i64 {
+    let mut fuel = rocket_fuel_equation(mass);
+    let mut fuel_vector: Vec<i64> = vec![fuel];
+
+    // calculating the fuel requirement of further fuel
+    loop {
+        let fuel_requirement = rocket_fuel_equation(fuel);
+        if fuel_requirement > 0 {
+            fuel = fuel_requirement;
+            fuel_vector.push(fuel);
+        }  else {
+            break;
+        }
+    }
+
+    let total_fuel_requirement: i64 = fuel_vector.iter().sum();
+
+    total_fuel_requirement
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn basic_example() {
-        assert_eq!(rocket_fuel_equation(12), 2);
-        assert_eq!(rocket_fuel_equation(14), 2);
-        assert_eq!(rocket_fuel_equation(1969), 654);
-        assert_eq!(rocket_fuel_equation(100756), 33583);
+        assert_eq!(rocket_fuel_equation_with_inner_fuel(12), 2);
+        assert_eq!(rocket_fuel_equation_with_inner_fuel(14), 2);
+        assert_eq!(rocket_fuel_equation_with_inner_fuel(1969), 966);
+        assert_eq!(rocket_fuel_equation_with_inner_fuel(100756), 50346);
     }
 }
